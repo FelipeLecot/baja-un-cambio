401c3133e385e27ec311acc982b1b4a5565cacd6
Felipe Lecot <47688222+FelipeLecot@users.noreply.github.com>
2023-12-05 10:31:40 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
 const app = express()
